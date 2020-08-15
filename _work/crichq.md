e5ad75b1c769bee065cff46a4097413079d659d3
Karthik <karthikeyan.rajendran@health.telstra.com>
2020-08-15 13:01:18 +1000

    adding articles and work content

---
+++ _work/crichq.md
@@ -0,0 +1,32 @@
+---
+title: 'CricHQ'
+excerpt: 'Web & mobile application design and development'
+coverImage: '/assets/work/crichq/cover.jpg'
+date: '2015-02-02T00:00:00.000Z'
+author:
+  name: Karthik
+  picture: '/assets/blog/authors/joe.jpeg'
+ogImage:
+  url: '/assets/blog/shell-grunt/cover.png'
+---
+
+CricHQ is a more of a platform than an app which automated the manual process of scoring and organising cricketing events at any scale and level. The initial user touch-point starts with the web app, where the user schedules and organises a tournament and then each of the scheduled matches are scored from within the mobile apps.
+
+### Role
+I was involved in the front-end development of the admin application, design of mobile app screens and the marketing website.
+
+### Design
+
+![](/assets/work/crichq/chq-prototypes.png)
+
+The designs were roughly translated from paper sketches to a low-fidelity prototypes after each iteration involving different users. I used tools like Balsamiq for prototyping and photoshop for the high-fidelity mocks. Designing for mobile screen mostly involved strategizing the asset delivery adhering to the each platform\u2019s interface guidelines.
+
+### Development
+
+![](/assets/work/crichq/chq-app.png)
+
+The webapp admin module was developed using [BackboneJS](http://backbonejs.org/), [MarionetteJS](https://marionettejs.com/) and [RequireJS](http://requirejs.org/) as the major frameworks and Rails as the backend which delivered REST services. Each sprint was different since I got to work across a range of modules:  social, competition, live matches and the marketing website design and development.
+
+![](/assets/work/crichq/chq-app1.png)
+
+The main challenges we faced during the development of the webapp were on the maintenance and reusability aspects. We used SCSS for styles development and broke down the scripts into multiple view layers to promote reusability of the components at large scale.
--- pages/articles.tsx
+++ pages/articles.tsx
@@ -1,17 +1,18 @@
-import { Layout, Container, Row } from '../components/Layout/index'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
 
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../types/post'
+import Head from 'next/head';
+import { Layout, Container, Row } from '../components/Layout/index';
+import MoreStories from '../components/more-stories';
+import HeroPost from '../components/hero-post';
+
+import { getAllPosts } from '../lib/api';
+import { CMS_NAME } from '../lib/constants';
+import Post from '../types/post';
 
 type Props = {
   allPosts: Post[]
 }
 
-const Recordings = ({ allPosts }: Props) => {
+const Articles = ({ allPosts }: Props) => {
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(1)
   return (
@@ -20,28 +21,30 @@ const Recordings = ({ allPosts }: Props) => {
         <Head>
           <title>Artciles - {CMS_NAME}</title>
         </Head>
-        <Container>
-          <Row>
-            <div className="col-12">
-              {heroPost && (
-                <HeroPost
-                  title={heroPost.title}
-                  coverImage={heroPost.coverImage}
-                  date={heroPost.date}
-                  author={heroPost.author}
-                  slug={heroPost.slug}
-                  excerpt={heroPost.excerpt}
-                />
-              )}
-              {morePosts.length > 0 && <MoreStories posts={morePosts} />}</div>
-          </Row>
-        </Container>
+        <div className="articles-page-body">
+          <Container>
+            <Row>
+              <div className="col-12">
+                {heroPost && (
+                  <HeroPost
+                    title={heroPost.title}
+                    coverImage={heroPost.coverImage}
+                    date={heroPost.date}
+                    author={heroPost.author}
+                    slug={heroPost.slug}
+                    excerpt={heroPost.excerpt}
+                  />
+                )}
+                {morePosts.length > 0 && <MoreStories posts={morePosts} />}</div>
+            </Row>
+          </Container>
+        </div>
       </Layout>
     </div>
   )
 }
 
-export default Recordings
+export default Articles
 
 export const getStaticProps = async () => {
   const allPosts = getAllPosts([
@@ -52,7 +55,6 @@ export const getStaticProps = async () => {
     'coverImage',
     'excerpt',
   ])
-
   return {
     props: { allPosts },
   }
