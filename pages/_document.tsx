d5c067fb1b277e8654fa007ab3df52e9c4075b63
Karthik <karthikeyan.rajendran@health.telstra.com>
2020-11-02 17:49:34 +1100

    GA integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,17 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WKPBK2DXL8"></script>
+        <script dangerouslySetInnerHTML={
+          {
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){window.dataLayer.push(arguments)}
+                  gtag("js", new Date());
+                  gtag("config", "G-WKPBK2DXL8");
+              `}
+        }>
+        </script>
         <body>
           <Main />
           <NextScript />
