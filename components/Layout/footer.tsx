c2ddf5d02f1a7513659e81c5390e09812b652f26
Karthik <karthikeyan.rajendran@health.telstra.com>
2021-02-13 15:28:38 +1100

    Icon ordering update

--- components/Layout/footer.tsx
+++ components/Layout/footer.tsx
@@ -2,26 +2,28 @@ import Link from 'next/link'
 import { Container, Row } from './index'
 
 const Footer = () => {
+
+  const yearVal = new Date().getFullYear();
   return (
     <Container>
       <Row>
         <section className="col-4">
           <strong className="label">contact</strong>
           <div className="social-icon-wrapper">
-            <a href="https://github.com/k97" target="_blank">
-              <img src="/assets/github-icon.svg" />
-            </a>
             <a href="https://twitter.com/k97co" target="_blank">
               <img src="/assets/twitter-icon.svg" />
             </a>
             <a href="mailto:karthik97live@gmail.com?subject=Hello Karthik!">
               <img src="/assets/mail-icon.svg" />
             </a>
+            <a href="https://github.com/k97" target="_blank">
+              <img src="/assets/github-icon.svg" />
+            </a>
             <a href="https://www.linkedin.com/in/rajendrankarthikeyan" target="_blank">
               <img src="/assets/linkedin-icon.svg" />
             </a>
           </div>
-          <span className="small-text">Copyright &copy; 2020. No Tracking</span>
+          <span className="small-text">Copyrights &copy; {yearVal}. No Tracking</span>
         </section>
       </Row>
     </Container>
