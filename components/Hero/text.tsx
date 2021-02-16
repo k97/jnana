06849243bbff8e6203ef30c3a736ca4dffbe1594
Karthik <karthikeyan.rajendran@health.telstra.com>
2021-02-16 21:37:20 +1100

    more info trigger on homepage

--- components/Hero/text.tsx
+++ components/Hero/text.tsx
@@ -7,15 +7,17 @@ const HeroText = () => {
         <strong >Hello! I am</strong>
         <h1 className="ff-hero">Karthik</h1>
 
-        <p className="p-large">Designer based in Melbourne, who likes to code, create experiences that makes people’s lives easier.
+        <p className="p-large">Designer based in Melbourne, who likes to code, create experiences that makes people’s lives easier.</p>
 
-          <Link href="/about">
-            <img src="/assets/next.svg" className="arrow-icon d-inline-block align-middle" />
-          </Link>
-        </p>
+        <Link href="/about">
+          <a className="btn">More about me</a>
+        </Link>
 
-        <p className="p-normal">See few of my selective <Link href="/work">work here</Link>, or If you want to read some interesting collation of <Link href="/articles">articles here</Link>.</p>
+        <p className="p-normal">
+          See few of my selective <Link href="/work"><a>work here</a></Link>,
+          or If you want to read some interesting collation of <Link href="/articles"><a>articles here</a></Link>.</p>
       </div>
+
       <div className="col-7">
         <img src="/assets/hero.svg" className="hero-static" />
       </div>
