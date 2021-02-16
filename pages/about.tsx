f4e9daff316cc6eca678024a318808eea01b6f63
Karthik <karthikeyan.rajendran@health.telstra.com>
2021-02-16 14:04:29 +1100

    About screen updates

--- pages/about.tsx
+++ pages/about.tsx
@@ -21,10 +21,14 @@ const About = () => {
             <div className="about-content col-6">
               <h1 className="ff-hero">About </h1>
               <p>My full name is Karthikeyan Rajendran and I like to think myself as part designer and a part coder. I have been tinkering with web ever since I started using it and I enjoy spending my time on designing and building products, both physical &amp; digital.</p>
+
               <p>Right now, I'm working as Product Designer at <a href="https://telstrahealth.com/" target="_blank">Telstra Health ↗</a> in Melbourne, designing and building digital products for virtual healthcare.
               </p>
-              <p>I share my perspectives on things that are interesting <a href="/articles">here</a> and if you are looking for my resume, <a href={GOOGLE_DRIVE_RESUME} target="_blank">it's here ↗</a>.
-              </p>
+
+              <p>Before Telstra Health, I was working with <a href="https://www.emirates.com/au/english/about-us/">Emirates Airlines ↗</a> across different teams to deliver products used by the flight crew and passengers. I started my foray as product designer at FastTrack360 cloud systems.</p>
+
+              <p>I share my perspectives on things that are interesting <a href="/articles">here</a> and if you are looking for my resume, <a href={GOOGLE_DRIVE_RESUME} target="_blank">it's here ↗</a>.</p>
+
             </div>
           </Row>
         </Container>
