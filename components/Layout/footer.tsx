abac86a4de119b782df9986f30de2e966d4165b1
Karthik <karthikeyan.rajendran@health.telstra.com>
2021-03-15 19:37:49 +1100

    Refactoring footer links & styles

--- components/Layout/footer.tsx
+++ components/Layout/footer.tsx
@@ -6,25 +6,25 @@ const Footer = () => {
   const yearVal = new Date().getFullYear();
   return (
     <Container maxW="container.xl" mt="100">
-      <SimpleGrid columns={5} spacing={0} className="social-icon-wrapper">
+      <SimpleGrid columns={5} spacing={0} maxWidth="400px">
         <Box>
           <a href="https://twitter.com/k97co" target="_blank">
-            <Image src="/assets/twitter-icon.svg" />
+            <Image src="/assets/twitter-icon.svg" className="footer-img" />
           </a>
         </Box>
         <Box>
           <a href="mailto:karthik97live@gmail.com?subject=Hello Karthik!">
-            <Image src="/assets/mail-icon.svg" />
+            <Image src="/assets/mail-icon.svg" className="footer-img" />
           </a>
         </Box>
         <Box>
           <a href="https://github.com/k97" target="_blank">
-            <Image src="/assets/github-icon.svg" />
+            <Image src="/assets/github-icon.svg" className="footer-img" />
           </a>
         </Box>
         <Box>
           <a href="https://www.linkedin.com/in/rajendrankarthikeyan" target="_blank">
-            <Image src="/assets/linkedin-icon.svg" />
+            <Image src="/assets/linkedin-icon.svg" className="footer-img" />
           </a>
         </Box>
       </SimpleGrid>
