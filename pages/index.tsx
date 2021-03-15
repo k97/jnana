280492723de610b40014445d14aab8ad55b1e7d2
Karthik <karthikeyan.rajendran@health.telstra.com>
2021-03-15 19:58:05 +1100

    Refactoring the new background for homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,15 +14,11 @@ const Index = () => {
       <style global jsx>{`
         html {
           height: 100%;
-          background-image: radial-gradient(circle at top left, rgba(174, 170, 203, 0.95) 0%, rgba(166, 202, 236, 0.95) 15%, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0) 100%),
-            radial-gradient(circle at top center, rgba(224, 130, 167, 0.95) 20%, rgba(255, 255, 255, 0) 50%),
-            radial-gradient(circle at top right, rgba(238, 90, 86, 0.95) 8%, rgba(254, 186, 77, 0.95) 20%, rgba(252, 224, 114, 0.95) 30%, rgba(238, 242, 215, 0.7) 45%, rgba(255, 255, 255, 0) 50%);
+          background-image: radial-gradient( circle 1158px at 6.4% 48.8%,  rgba(195,241,245,1) 0%, rgba(255,255,255,1) 90.2% );
           background-position: top center;
           background-size: cover;
           background-repeat: no-repeat;
           background-attachment: fixed;
-          height: 100%;
-          background-image: radial-gradient( circle 1158px at 6.4% 48.8%,  rgba(195,241,245,1) 0%, rgba(255,255,255,1) 90.2% );
         }
         .header-background {
           border: none;
