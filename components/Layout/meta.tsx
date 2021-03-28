d295c133f268559315aefb3c02fed54180b7ef36
Karthik <karthik97live@gmail.com>
2021-03-28 18:53:18 +1100

    New meta tags and about content tweaks

--- components/Layout/meta.tsx
+++ components/Layout/meta.tsx
@@ -1,44 +1,37 @@
 import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL, CMS_DESCRIPTION } from '../../lib/constants'
+import { CMS_NAME, HOME_OG_IMAGE_URL, CMS_URL, CMS_DESCRIPTION } from '../../lib/constants'
 
 const Meta = () => {
   return (
     <Head>
+      <meta name="description" content={CMS_DESCRIPTION} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={CMS_URL} />
+      <meta property="og:title" content={CMS_NAME} />
+      <meta property="og:description" content={CMS_DESCRIPTION} />
+      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={CMS_URL} />
+      <meta property="twitter:title" content={CMS_NAME} />
+      <meta property="twitter:description" content={CMS_DESCRIPTION} />
+      <meta property="twitter:image" content={HOME_OG_IMAGE_URL} />
+
       <meta name="google-site-verification" content="8OLExMes5srC_J0gow9IorjUOUD3smQxKkNNNJhci-A" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon_io/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon_io/favicon-16x16.png"
-      />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
       <link rel="manifest" href="/favicon_io/site.webmanifest" />
       <link rel="shortcut icon" href="/favicon_io/favicon.ico" />
-      <meta
-        name="description"
-        content={CMS_DESCRIPTION}
-      />
 
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="msapplication-TileColor" content="#000000" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
+      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
     </Head>
   )
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,8 @@
-export const CMS_NAME = "Karthik - Digital Product Designer's folio"
+export const CMS_NAME = "Karthik - Digital Product Designer"
 export const CMS_DESCRIPTION = "Personal portfolio of Karthik — a designer bridging the gap between digital products and people using them."
-export const HOME_OG_IMAGE_URL =
-  'https://og-image.now.sh/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg'
+export const HOME_OG_IMAGE_URL = 'https://k97static.s3-ap-southeast-2.amazonaws.com/v8/metax.png'
+export const CMS_URL = "https://www.rkarthik.co/"
 
 export const GOOGLE_DRIVE_RESUME = 'https://drive.google.com/file/d/15Hl6bQv_3ig5kjc_RkZuf3nf0fabrxZE/view?usp=sharing'
+
 export const GOOGLE_DRIVE_RESUME_DOWNLOAD= 'https://drive.google.com/uc?id=15Hl6bQv_3ig5kjc_RkZuf3nf0fabrxZE&export=download'
