e01ab84520a37725e14519f629333fd83ab5c53b
Karthik <karthikeyan.rajendran@health.telstra.com>
2021-03-29 20:35:42 +1100

    Tweaking hero image on the home screen

---
+++ components/Hero/hero-image.tsx
@@ -0,0 +1,34 @@
+export const HeroImage = () => {
+  return (
+    <svg width="688" height="688" viewBox="0 0 688 688" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_d)">
+        <circle cx="344" cy="335" r="306" fill="#397ED3" />
+      </g>
+      <path opacity="0.4" d="M594.002 347.335C594.002 485.407 482.073 597.336 344.002 597.336C205.931 597.336 94.0018 485.407 94.0018 347.335C94.0018 209.264 261.931 461.336 400.002 461.336C538.073 461.336 594.002 209.264 594.002 347.335Z" fill="url(#paint0_linear)" />
+      <path opacity="0.4" d="M594 354.524C594 488.44 482.072 597 344.001 597C205.93 597 94.0018 488.44 94.0018 354.524C94.0018 220.608 91.9306 469.942 230.001 469.942C368.072 469.942 594 220.608 594 354.524Z" fill="url(#paint1_linear)" />
+      <path d="M436 230C436 287.99 388.99 335 331 335C273.01 335 226 287.99 226 230C226 172.01 273.01 125 331 125C388.99 125 436 172.01 436 230Z" fill="url(#paint2_linear)" />
+      <defs>
+        <filter id="filter0_d" x="0" y="0" width="688" height="688" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
+          <feOffset dy="9" />
+          <feGaussianBlur stdDeviation="19" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0.258333 0 0 0 0 0.258333 0 0 0 0 0.258333 0 0 0 0.25 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
+        </filter>
+        <linearGradient id="paint0_linear" x1="321.262" y1="124.336" x2="375" y2="170" gradientUnits="userSpaceOnUse">
+          <stop offset="1" stop-color="#D8EDFA" />
+        </linearGradient>
+        <linearGradient id="paint1_linear" x1="321.261" y1="138.235" x2="321.261" y2="623.188" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#697277" />
+          <stop offset="1" stop-color="#D8EDFA" />
+        </linearGradient>
+        <linearGradient id="paint2_linear" x1="361" y1="305" x2="416" y2="247" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#FFC700" />
+          <stop offset="1" stop-color="#FE6955" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
